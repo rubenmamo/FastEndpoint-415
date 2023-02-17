68cc5f042829f9005eaaf423683926e601187374
Mamo, Ruben <RubenMamo@kpmg.com.mt>
2023-02-17 16:09:46 +0100

    First commit

---
+++ FastEndpointTest/src/FastEndpointTest/Endpoints/PostWithHeaderEndpoint/Endpoint.cs
@@ -0,0 +1,21 @@
+﻿namespace FastEndpointTest.Endpoints.PostWithHeader
+{
+    public class Endpoint : Endpoint<Request, EmptyResponse>
+    {
+        public override void Configure()
+        {
+            Post("/route/path/here");
+            AllowAnonymous();
+        }
+
+        public override Task HandleAsync(Request r, CancellationToken c)
+        {
+            return SendAsync(Response);
+        }
+    }
+    public class Request
+    {
+        [FromHeader("some-header")]
+        public string SomeHeader { get; set; }
+    }
+}
